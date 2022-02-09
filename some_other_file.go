041e6fb6484fd70c26985693d1fd87cdb06e6002
NickMahnke <88862436+NickMahnke@users.noreply.github.com>
2022-02-09 12:07:46 -0800

    Update some_other_file.go

--- some_other_file.go
+++ some_other_file.go
@@ -83,7 +83,11 @@ blah
 
 
 
-
+resp, err := grequests.get("http://sendgrid.com/pants?type=trousers",
+			&grequests.RequestOptions{
+				JSON:   map[string]string{"token": req.Header.Get("Some-Other-Token-Two")},
+				Headers: map[string]string{"Some-Other-Token": token},
+			})
 
 
 
