712aa3c1400cf3d57b5ca1370ffa67f3e934fb35
NickMahnke <88862436+NickMahnke@users.noreply.github.com>
2024-03-01 13:55:12 -0800

    Update anotherfile.go

--- subdirectory/anotherfile.go
+++ subdirectory/anotherfile.go
@@ -1,7 +1,5 @@
 stuff
 
-resp, gAPIErr := grequests.Get(fmt.Sprintf("https://example.com/"), &grequests.RequestOptions{InsecureSkipVerify: true})
-
 resp, gAPIErr := grequests.Get(fmt.Sprintf("https://att.com/"), &grequests.RequestOptions{InsecureSkipVerify: true})
 
 resp, gAPIErr := grequests.Get(fmt.Sprintf("https://pge.com/"), &grequests.RequestOptions{InsecureSkipVerify: true})
